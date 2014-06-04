33b4e626357e8052101e5738553c64974c6caede
Stellan Lagerstrom <stellanl@akvo.org>
2014-06-04 05:43:10 +0200

    [#91]Modify exported gradle build scripts to reference http-request library. Fix string resource translation mismatch.

--- android/AkvoRSR/src/org/akvo/rsr/up/ProjectListActivity.java
+++ android/AkvoRSR/src/org/akvo/rsr/up/ProjectListActivity.java
@@ -31,6 +31,7 @@ import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -86,6 +87,7 @@ public class ProjectListActivity extends ListActivity {
                 } else {
                     searchField.setVisibility(View.VISIBLE);
                     searchField.requestFocus();
+                    searchField.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
                 }
             }
         });
