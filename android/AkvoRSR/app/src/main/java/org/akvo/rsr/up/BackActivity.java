9e5b4df1cdecdff3db83e767cdaf7ff4ef0d352b
valeria <valerie.cardoso@gmail.com>
2020-12-07 17:46:34 +0100

    [#211] cleanup back activity

--- android/AkvoRSR/app/src/main/java/org/akvo/rsr/up/BackActivity.java
+++ android/AkvoRSR/app/src/main/java/org/akvo/rsr/up/BackActivity.java
@@ -5,9 +5,6 @@ import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//import org.akvo.flow.R;
-
-@SuppressWarnings("deprecation")
 public abstract class BackActivity extends AppCompatActivity {
 
     @Override
@@ -15,21 +12,16 @@ public abstract class BackActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         if (getSupportActionBar() != null) {
-            //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
-//            getSupportActionBar().setIcon(R.drawable.ic_arrow_back_white_48dp);
         }
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
-
 }
