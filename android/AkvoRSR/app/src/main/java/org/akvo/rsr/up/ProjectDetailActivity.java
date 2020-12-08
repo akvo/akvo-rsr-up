50ffcbec641cbd39939ce378371515634864ac3c
valeria <valerie.cardoso@gmail.com>
2020-12-08 16:10:53 +0100

    [#211] cleanup

--- android/AkvoRSR/app/src/main/java/org/akvo/rsr/up/ProjectDetailActivity.java
+++ android/AkvoRSR/app/src/main/java/org/akvo/rsr/up/ProjectDetailActivity.java
@@ -16,6 +16,26 @@
 
 package org.akvo.rsr.up;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.res.Resources;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
 import org.akvo.rsr.up.dao.RsrDbAdapter;
 import org.akvo.rsr.up.domain.Project;
 import org.akvo.rsr.up.service.GetProjectDataService;
@@ -23,27 +43,7 @@ import org.akvo.rsr.up.util.ConstantUtil;
 import org.akvo.rsr.up.util.DialogUtil;
 import org.akvo.rsr.up.util.SettingsUtil;
 import org.akvo.rsr.up.util.ThumbnailUtil;
-
-import android.net.Uri;
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.res.Resources;
+import org.jetbrains.annotations.NotNull;
 
 public class ProjectDetailActivity extends AppCompatActivity {
 
@@ -56,9 +56,7 @@ public class ProjectDetailActivity extends AppCompatActivity {
     private TextView draftCountView;
     private TextView resultCountView;
 	private ImageView projImage;
-    private Button btnUpdates;
-	private Button btnAddUpdate;
-    private Button btnResults;
+	private Button btnResults;
     private Button btnRefresh;
     private LinearLayout inProgress;
     private ProgressBar mInProgressBar;
@@ -66,8 +64,7 @@ public class ProjectDetailActivity extends AppCompatActivity {
 	private boolean mDebug;
 
 	private RsrDbAdapter mDba;
-    private BroadcastReceiver mBroadRec;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -84,53 +81,41 @@ public class ProjectDetailActivity extends AppCompatActivity {
 					.getString(ConstantUtil.PROJECT_ID_KEY) : null;
 		}
 
-		//find the fields
-		projTitleLabel		= (TextView) findViewById(R.id.text_proj_detail_title);
-		projLocationText 	= (TextView) findViewById(R.id.text_proj_location);
-		projSummaryText		= (TextView) findViewById(R.id.text_proj_summary);
-		projImage 			= (ImageView) findViewById(R.id.image_proj_detail);
-		publishedCountView 	= (TextView) findViewById(R.id.text_proj_detail_published_count);
-        draftCountView      = (TextView) findViewById(R.id.text_proj_detail_draft_count);
-        resultCountView     = (TextView) findViewById(R.id.text_proj_detail_result_count);
-        inProgress          = (LinearLayout) findViewById(R.id.proj_detail_progress);
-        mInProgressBar      = (ProgressBar) findViewById(R.id.progress_bar);
-        mInProgressWhat     = (TextView) findViewById(R.id.progress_title);
+		projTitleLabel = (TextView) findViewById(R.id.text_proj_detail_title);
+		projLocationText = (TextView) findViewById(R.id.text_proj_location);
+		projSummaryText = (TextView) findViewById(R.id.text_proj_summary);
+		projImage = (ImageView) findViewById(R.id.image_proj_detail);
+		publishedCountView = (TextView) findViewById(R.id.text_proj_detail_published_count);
+		draftCountView = (TextView) findViewById(R.id.text_proj_detail_draft_count);
+		resultCountView = (TextView) findViewById(R.id.text_proj_detail_result_count);
+		inProgress = (LinearLayout) findViewById(R.id.proj_detail_progress);
+		mInProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
+		mInProgressWhat = (TextView) findViewById(R.id.progress_title);
 
-		//Activate buttons
-		btnUpdates = (Button) findViewById(R.id.btn_view_updates);
-		btnUpdates.setOnClickListener( new View.OnClickListener() {
-			public void onClick(View view) {
-				Intent i = new Intent(view.getContext(), UpdateListActivity.class);
-				i.putExtra(ConstantUtil.PROJECT_ID_KEY, projId);
-				startActivity(i);
-			}
+		Button btnUpdates = (Button) findViewById(R.id.btn_view_updates);
+		btnUpdates.setOnClickListener(view -> {
+			Intent i = new Intent(view.getContext(), UpdateListActivity.class);
+			i.putExtra(ConstantUtil.PROJECT_ID_KEY, projId);
+			startActivity(i);
 		});
-		btnAddUpdate = (Button) findViewById(R.id.btn_add_update);
-		btnAddUpdate.setOnClickListener( new View.OnClickListener() {
-			public void onClick(View view) {
-				Intent i = new Intent(view.getContext(), UpdateEditorActivity.class);
-				i.putExtra(ConstantUtil.PROJECT_ID_KEY, projId);
-				startActivity(i);
-			}
+		Button btnAddUpdate = (Button) findViewById(R.id.btn_add_update);
+		btnAddUpdate.setOnClickListener(view -> {
+			Intent i = new Intent(view.getContext(), UpdateEditorActivity.class);
+			i.putExtra(ConstantUtil.PROJECT_ID_KEY, projId);
+			startActivity(i);
 		});
 		
         //Results
         btnResults = (Button) findViewById(R.id.btn_view_results);
-        btnResults.setOnClickListener( new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent i = new Intent(view.getContext(), ResultListActivity.class);
-                i.putExtra(ConstantUtil.PROJECT_ID_KEY, projId);
-                startActivity(i);
-            }
-        });
+        btnResults.setOnClickListener(view -> {
+			Intent i = new Intent(view.getContext(), ResultListActivity.class);
+			i.putExtra(ConstantUtil.PROJECT_ID_KEY, projId);
+			startActivity(i);
+		});
 
         //Refresh for this project only (speeds up things if very many projects)
         btnRefresh = (Button) findViewById(R.id.btn_refresh_proj);
-        btnRefresh.setOnClickListener( new View.OnClickListener() {
-            public void onClick(View view) {
-                startGetProjectsService();
-            }
-        });
+        btnRefresh.setOnClickListener(view -> startGetProjectsService());
 
 		
 		mDba = new RsrDbAdapter(this);
@@ -138,10 +123,9 @@ public class ProjectDetailActivity extends AppCompatActivity {
 		// Show the Up button in the action bar.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         
-        //register a listener for completion broadcasts
         IntentFilter f = new IntentFilter(ConstantUtil.PROJECTS_FETCHED_ACTION);
         f.addAction(ConstantUtil.PROJECTS_PROGRESS_ACTION);
-        mBroadRec = new ResponseReceiver();
+		BroadcastReceiver mBroadRec = new ResponseReceiver();
         LocalBroadcastManager.getInstance(this).registerReceiver(mBroadRec, f);
 
 	}
@@ -156,7 +140,7 @@ public class ProjectDetailActivity extends AppCompatActivity {
     		    return;
     		}
     		if (mDebug) {
-    			projTitleLabel.setText("[" + projId + "] " + project.getTitle());
+    			projTitleLabel.setText(String.format("[%s] %s", projId, project.getTitle()));
     		} else {
     			projTitleLabel.setText(project.getTitle());
     		}
@@ -175,18 +159,11 @@ public class ProjectDetailActivity extends AppCompatActivity {
     			loc = loc.substring(0, loc.length()-2);
     		}
     
-    		//TODO check against 0,0 too?
     		if (project.getLatitude() != null &&
     			project.getLongitude() != null ) {
     			loc += "\nLatitude " + project.getLatitude() +
     			        " Longitude " + project.getLongitude();
-    			projLocationText.setOnClickListener(
-    				new OnClickListener() {
-    					@Override
-    					public void onClick(View v) {
-    						launchLatLonIntent();
-    					}
-    				});
+    			projLocationText.setOnClickListener(v -> launchLatLonIntent());
     		} else {
     			projLocationText.setOnClickListener(null);
     		}
@@ -194,16 +171,16 @@ public class ProjectDetailActivity extends AppCompatActivity {
     		projLocationText.setText(loc);
     		projSummaryText.setText(project.getSummary());
     		
-    		int [] stateCounts = {0,0,0};
+    		int [] stateCounts;
     		stateCounts = mDba.countAllUpdatesFor(projId);
     		Resources res = getResources();
-    		publishedCountView.setText(Integer.toString(stateCounts[2]) + res.getString(R.string.count_published));
-            draftCountView.setText(Integer.toString(stateCounts[0]) + res.getString(R.string.count_draft));
+    		publishedCountView.setText(String.format("%d%s", stateCounts[2], res.getString(R.string.count_published)));
+            draftCountView.setText(String.format("%d%s", stateCounts[0], res.getString(R.string.count_draft)));
             draftCountView.setVisibility(stateCounts[0] > 0 ? View.VISIBLE : View.GONE);
 
             int rc = mDba.countResultsFor(projId);
             int ic = mDba.countIndicatorsFor(projId);
-            resultCountView.setText(Integer.toString(rc) + res.getString(R.string.count_results) + ", " + Integer.toString(ic) + res.getString(R.string.count_indicators));
+            resultCountView.setText(String.format("%d%s, %d%s", rc, res.getString(R.string.count_results), ic, res.getString(R.string.count_indicators)));
             resultCountView.setVisibility(rc > 0 ? View.VISIBLE : View.GONE);
             btnResults.setEnabled(rc > 0);
     
@@ -215,28 +192,14 @@ public class ProjectDetailActivity extends AppCompatActivity {
 
 	}
 	
-	@Override
-	protected void onPause() {
-		super.onPause();
-	}
-	
-
-	@Override
-	protected void onDestroy() {
-		super.onDestroy();
-	}
-
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        outState.putString(ConstantUtil.PROJECT_ID_KEY, projId);
-        // Always call the superclass so it can save the view hierarchy state
+    protected void onSaveInstanceState(@NotNull Bundle outState) {
         super.onSaveInstanceState(outState);
-    }
+		outState.putString(ConstantUtil.PROJECT_ID_KEY, projId);
+	}
 
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.project_detail, menu);
 		return true;
 	}
@@ -246,7 +209,6 @@ public class ProjectDetailActivity extends AppCompatActivity {
 	    switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
-//              NavUtils.navigateUpFromSameTask(this);
                 return true;
 	        case R.id.action_settings:
 				Intent intent = new Intent(this, SettingsActivity.class);
@@ -257,7 +219,6 @@ public class ProjectDetailActivity extends AppCompatActivity {
 	    }
 	}
 
-	
 	private void launchLatLonIntent() {
 		if (project != null &&
 		    project.getLatitude() != null &&
@@ -268,30 +229,27 @@ public class ProjectDetailActivity extends AppCompatActivity {
 			startActivity(intent);
 		}
 	}
+
     /**
      * starts the service fetching new project data
      */
     private void startGetProjectsService() {
-        if (GetProjectDataService.isRunning(this)) { //unlikely as button should be disabled
+        if (GetProjectDataService.isRunning(this)) {
             return; //only one at a time
         }
         //disable button
         btnRefresh.setEnabled(false);
-        //start a service       
         Intent i = new Intent(this, GetProjectDataService.class);
         i.putExtra(ConstantUtil.PROJECT_ID_KEY, projId);
         getApplicationContext().startService(i);
         
-        //start progress animation
         inProgress.setVisibility(View.VISIBLE);
         mInProgressBar.setProgress(0);
         mInProgressWhat.setText("=====");
     }
 
-    
     /**
      * handles result of refresh service
-     * @param intent
      */
     private void onFetchFinished(Intent intent) {
         // Hide in-progress indicators
@@ -313,19 +271,24 @@ public class ProjectDetailActivity extends AppCompatActivity {
     
     /**
      * updates the progress bar etc as fetch progresses
-     * @param phase
-     * @param done
-     * @param total
      */
     private void onFetchProgress(int phase, int done, int total) {
         mInProgressBar.setMax(total);
         mInProgressBar.setProgress(done);
-        switch (phase) {
-            case  0:mInProgressWhat.setText("Projects");break;
-            case  1:mInProgressWhat.setText("Updates");break;
-            case  2:mInProgressWhat.setText("Photos");break;
-            default:mInProgressWhat.setText("???");break;
-        }
+		switch (phase) {
+			case 0:
+				mInProgressWhat.setText(R.string.progress_projects);
+				break;
+			case 1:
+				mInProgressWhat.setText(R.string.progress_updates);
+				break;
+			case 2:
+				mInProgressWhat.setText(R.string.progress_photos);
+				break;
+			default:
+				mInProgressWhat.setText("???");
+				break;
+		}
     }
     
     /**
@@ -337,16 +300,17 @@ public class ProjectDetailActivity extends AppCompatActivity {
         private ResponseReceiver() {
         }
         
-        // Called when the BroadcastReceiver gets an Intent it's registered to receive
         public void onReceive(Context context, Intent intent) {
-            if (intent.getAction() == ConstantUtil.PROJECTS_FETCHED_ACTION)
-                onFetchFinished(intent);
-            else if (intent.getAction() == ConstantUtil.PROJECTS_PROGRESS_ACTION)
-                onFetchProgress(intent.getExtras().getInt(ConstantUtil.PHASE_KEY, 0),
-                                intent.getExtras().getInt(ConstantUtil.SOFAR_KEY, 0),
-                                intent.getExtras().getInt(ConstantUtil.TOTAL_KEY, 100));
+			switch (intent.getAction()) {
+				case ConstantUtil.PROJECTS_FETCHED_ACTION:
+					onFetchFinished(intent);
+					break;
+				case ConstantUtil.PROJECTS_PROGRESS_ACTION:
+					onFetchProgress(intent.getExtras().getInt(ConstantUtil.PHASE_KEY, 0),
+							intent.getExtras().getInt(ConstantUtil.SOFAR_KEY, 0),
+							intent.getExtras().getInt(ConstantUtil.TOTAL_KEY, 100));
+					break;
+			}
         }
     }
-
-
 }
