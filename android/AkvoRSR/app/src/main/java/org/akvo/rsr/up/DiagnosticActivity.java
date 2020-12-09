86417d46a92bd7c135f33630e9a35b09256c7a5b
valeria <valerie.cardoso@gmail.com>
2020-12-09 10:08:39 +0100

    [#211] diagnostic activity test and cleanup

--- android/AkvoRSR/app/src/main/java/org/akvo/rsr/up/DiagnosticActivity.java
+++ android/AkvoRSR/app/src/main/java/org/akvo/rsr/up/DiagnosticActivity.java
@@ -16,51 +16,29 @@
 
 package org.akvo.rsr.up;
 
-import java.util.List;
-
-import org.akvo.rsr.up.dao.RsrDbAdapter;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.StatFs;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
 import android.widget.TextView;
-import android.database.Cursor;
 
-import androidx.appcompat.app.AppCompatActivity;
+import org.akvo.rsr.up.dao.RsrDbAdapter;
 
-public class DiagnosticActivity extends AppCompatActivity {
+import java.util.List;
+
+public class DiagnosticActivity extends BackActivity {
 
 	private TextView mTextView;
-	private Button mBtnUpdates;
-	private Button mBtnClearDb;
 
-	private RsrDbAdapter mDb;
+    private RsrDbAdapter mDb;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		//get the look
 		setContentView(R.layout.activity_diagnostic);
 		//find the fields
 		mTextView = (TextView) findViewById(R.id.text_field);
 		//Activate buttons
-/*
-		mBtnUpdates = (Button) findViewById(R.id.btn_diag_a);
-		mBtnUpdates.setOnClickListener( new View.OnClickListener() {
-			public void onClick(View view) {//delete image cache files
-				clearCache(DiagnosticActivity.this);
-			}
-		});
-		mBtnClearDb = (Button) findViewById(R.id.btn_diag_b);
-		mBtnClearDb.setOnClickListener( new View.OnClickListener() {
-			public void onClick(View view) {
-				clearData();
-			}
-		});
- */
 		mDb = new RsrDbAdapter(this);
 		mDb.open();
 	}
@@ -71,7 +49,7 @@ public class DiagnosticActivity extends AppCompatActivity {
 		super.onResume();
 
         Cursor users = mDb.listAllUsers();
-        mTextView.append("\n\nUsers in db: " + String.valueOf(users.getCount()));
+        mTextView.append("\n\nUsers in db: " + users.getCount());
         while (users.moveToNext())
             mTextView.append("\n[" + users.getString(users.getColumnIndex(RsrDbAdapter.PK_ID_COL)) +
                              "] "  + users.getString(users.getColumnIndex(RsrDbAdapter.FIRST_NAME_COL)) +
@@ -79,49 +57,43 @@ public class DiagnosticActivity extends AppCompatActivity {
         users.close();
 
         List<String>u = mDb.getMissingUsersList();
-        mTextView.append("\n\nMissing users in db: " + String.valueOf(u.size()));
+        mTextView.append("\n\nMissing users in db: " + u.size());
         for (String id:u) {
             mTextView.append("\n["+id+"] ");
         }
 
         Cursor orgs = mDb.listAllOrgs();
-        mTextView.append("\n\nOrgs in db: " + String.valueOf(orgs.getCount()));
+        mTextView.append("\n\nOrgs in db: " + orgs.getCount());
         while (orgs.moveToNext())
             mTextView.append("\n["+orgs.getString(orgs.getColumnIndex(RsrDbAdapter.PK_ID_COL))+"] "+orgs.getString(orgs.getColumnIndex(RsrDbAdapter.NAME_COL))+" ");
         orgs.close();
 
         List<String>o = mDb.getMissingOrgsList();
-        mTextView.append("\n\nMissing orgs in db: " + String.valueOf(o.size()));
+        mTextView.append("\n\nMissing orgs in db: " + o.size());
         for (String id:o) {
             mTextView.append("\n["+id+"] ");
         }
         Cursor b = mDb.listAllProjects();
-        mTextView.append("\n\nProjects in db: " + String.valueOf(b.getCount()));
-//        while (b.moveToNext())
-//            mTextView.append("\n["+b.getString(b.getColumnIndex(RsrDbAdapter.PK_ID_COL))+"] " + b.getInt(b.getColumnIndex(RsrDbAdapter.LAST_FETCH_COL))+" ");
+        mTextView.append("\n\nProjects in db: " + b.getCount());
         b.close();
         Cursor b2 = mDb.listVisibleProjects();
-        mTextView.append("\n\nVisible projects in db: " + String.valueOf(b2.getCount()));
+        mTextView.append("\n\nVisible projects in db: " + b2.getCount());
         b2.close();
         Cursor c = mDb.listAllUpdates();
-        mTextView.append("\n\nUpdates in db: " + String.valueOf(c.getCount()));
+        mTextView.append("\n\nUpdates in db: " + c.getCount());
         c.close();
         Cursor a = mDb.listAllCountries();
-        mTextView.append("\n\nCountries in db: " + String.valueOf(a.getCount()));
+        mTextView.append("\n\nCountries in db: " + a.getCount());
         while (a.moveToNext())
             mTextView.append("\n[" + a.getString(a.getColumnIndex(RsrDbAdapter.PK_ID_COL))+"] " + a.getString(a.getColumnIndex(RsrDbAdapter.NAME_COL)) + " ");
         a.close();
 
-        mTextView.append("\n\nResults in db: " + String.valueOf(mDb.countResults()));
+        mTextView.append("\n\nResults in db: " + mDb.countResults());
 
-        mTextView.append("\n\nIndicators in db: " + String.valueOf(mDb.countIndicators()));
+        mTextView.append("\n\nIndicators in db: " + mDb.countIndicators());
 
-        mTextView.append("\n\nPeriods in db: " + String.valueOf(mDb.countPeriods()));
+        mTextView.append("\n\nPeriods in db: " + mDb.countPeriods());
 
-//		Cursor d = mDb.listAllUpdatesFor("609");
-//		mTextView.append("\nUpdates in db for 609: " + String.valueOf(d.getCount())+"\n");
-//		d.close();
-		
 		StatFs stat = new StatFs(Environment.getExternalStorageDirectory().getPath());
 		double sdAvailSize = (double)stat.getAvailableBlocks()
 		                   * (double)stat.getBlockSize();
@@ -138,29 +110,4 @@ public class DiagnosticActivity extends AppCompatActivity {
 		}
 		super.onDestroy();
 	}
-
-	@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.diagnostics, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-/*
-        case R.id.action_clear_diagnostics:
-            return true;
-        case R.id.action_settings:
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-            return true;
-            */
-        default:
-            return super.onOptionsItemSelected(item);
-        }
-
-    }
-
 }
